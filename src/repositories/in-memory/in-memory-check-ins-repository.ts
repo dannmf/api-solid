1734c9be320bb1e3e2c83e3091dd4e36e0fafc2e
daniel.manoel <daniellfmanoel@gmail.com>
2024-09-02 10:25:35 -0300

    🧪 using tdd to validate check ins twice in same day implements

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -4,6 +4,18 @@ import { randomUUID } from "node:crypto";
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
     public items: CheckIn[] = []
+
+    async findByUserIdOnDate(userId: string, date: Date) {
+        const checkInSameDate = this.items.find((checkIn) => checkIn.user_id === userId,)
+        
+        if(!checkInSameDate){
+            return null
+        }
+
+
+        return checkInSameDate
+    }
+
     async create(data: Prisma.CheckInUncheckedCreateInput) {
         const checkIn = {
             id: randomUUID(),
